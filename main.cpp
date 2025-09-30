82f608090f2d4576d06a47a0c9df4b480f5b88bc
thelunartear <thelunartears@gmail.com>
2025-09-30 09:51:20 +0900

    주석 추가

--- main.cpp
+++ main.cpp
@@ -7,6 +7,9 @@ vector<long long> arr;
 
 int main()
 {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+
 	long long n;
 	cin >> n;
 	for (long long i = 0; i < n; i++)
@@ -16,12 +19,13 @@ int main()
 		vec.push_back(x);
 	}
 	
-	// 원소와 같거나 큰 2의 제곱수 구해서 array 배열에 삽입하기
+	// 원소와 같거나 큰 2의 제곱수 구해서 arr 배열에 삽입하기
 	for (long long v : vec)
 	{
 		long long tmp = v;
 
-		// tmp가 2의 제곱수일 경우도 고려
+		// 2의 제곱수를 구하기 + 숫자 범위가 매우 크므로 비트연산자로 구현
+		// tmp가 2의 제곱수일 경우 값을 그대로 반환하기 위해 --연산자 사용
 		tmp--;
 
 		tmp |= tmp >> 1;
@@ -31,6 +35,7 @@ int main()
 		tmp |= tmp >> 16;
 		tmp |= tmp >> 32;
 
+		// -1한 값을 다시 +1해서 롤백하기
 		long long ans = tmp + 1;
 		arr.push_back(ans);
 	}
